602d6f5c26d73f299e035b287970c89c746affe1
Sniperrifle2004 <sniperrifle2004+vba-rr@gmail.com>
2016-04-28 01:44:15 +0200

    Remove a pad that should never have been added
    It's hard, but true! I doubt it would have had
    the desired effect and always writing a u32
    should be better suited for the next couple
    of years (unless someone wanted to artificially
    start a run before the epoch). Maybe some form
    of check can be done to read 64bit timestamps,
    but it has to be better than this.

--- src/gb/V7/GB.cpp
+++ src/gb/V7/GB.cpp
@@ -1911,7 +1911,7 @@ void gbWriteSaveMBC3(const char *name, bool extendedSave)
 	{
 		fwrite(&gbDataMBC3.mapperSeconds,
 		       1,
-		       10 * sizeof(int32) + sizeof(u32) + 4,
+		       10 * sizeof(int32) + sizeof(u32),
 		       gzFile);
 	}
 
@@ -2029,8 +2029,8 @@ bool gbReadSaveMBC3(const char * name)
 	else if ((gbRom[0x147] == 0xf) || (gbRom[0x147] == 0x10)) {
 		read = gzread(gzFile,
 			&gbDataMBC3.mapperSeconds,
-			sizeof(int) * 10 + sizeof(time_t) + 4);
-		if (read != (sizeof(int) * 10 + sizeof(time_t) + 4) && read != 0) {
+			sizeof(int32) * 10 + sizeof(u32));
+		if (read != (sizeof(int32) * 10 + sizeof(u32)) && read != 0) {
 			systemMessage(MSG_FAILED_TO_READ_RTC, N_("Failed to read RTC from save game %s (continuing)"),
 				name);
 			res = false;
